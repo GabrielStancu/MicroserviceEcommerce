f2c4683c883320f78bb153f6f0f53d32da1efe8b
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-08-28 15:21:32 +0300

    Add fluent validation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -5,18 +5,33 @@ public record CreateProductCommand(string Name, List<string> Category, string De
 
 public record CreateProductResult(Guid Id);
 
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
 internal class CreateProductCommandHandler 
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     private readonly IDocumentSession _session;
+    private readonly ILogger<CreateProductCommand> _logger;
 
-    public CreateProductCommandHandler(IDocumentSession session)
+    public CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommand> logger)
     {
         _session = session;
+        _logger = logger;
     }
 
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("CreateProductCommandHandler.Handle called with command {@Command}", command);
+
         var product = new Product
         {
             Name = command.Name,
