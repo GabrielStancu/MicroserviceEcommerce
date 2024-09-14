cc8c7683d4d1ce3c484186d3c96195d0b44034d4
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-09-14 17:05:18 +0300

    Delete basket handler

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,5 +1,23 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async  Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // TODO: delete the basket from the database and the cache
+
+        return new DeleteBasketResult(true);
+    }
 }
