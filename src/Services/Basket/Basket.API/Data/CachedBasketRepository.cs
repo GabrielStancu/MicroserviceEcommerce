a2449f3984e06b1926d056fada9a1ce4ed9b302f
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-09-24 18:41:50 +0300

    Add cached basket repository as simple proxy

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Data;
+
+public class CachedBasketRepository : IBasketRepository
+{
+    private readonly IBasketRepository _repository;
+
+    public CachedBasketRepository(IBasketRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return await _repository.GetBasket(userName, cancellationToken);
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        return await _repository.StoreBasket(basket, cancellationToken);
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return await _repository.DeleteBasket(userName, cancellationToken);
+    }
+}
