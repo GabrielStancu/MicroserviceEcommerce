35614625f976f52ac3693765739a25787e1b8676
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-10-30 11:26:50 +0200

    Initializers for customer and customer id, domain exception

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -2,6 +2,21 @@
 
 public class Customer : Entity<CustomerId>
 {
-    public string Name => default!;
-    public string Email => default!;
+    public string Name { get; private init; } = default!;
+    public string Email { get; private init; } = default!;
+
+    public static Customer Create(CustomerId customerId, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+        var customer = new Customer
+        {
+            Id = customerId,
+            Name = name,
+            Email = email
+        };
+
+        return customer;
+    }
 }
