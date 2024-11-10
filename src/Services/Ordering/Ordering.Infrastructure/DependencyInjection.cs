42ce2bd9fe11cf54cab770f4d186001d2c965311
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-11-10 21:37:28 +0200

    Register db context, global using

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -7,6 +7,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
         return services;
     }
 }
