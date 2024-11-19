4320a00e3a0b309f3234bd7f7d217062d5b75e16
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-11-20 01:47:40 +0200

    Create Order command and handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Core.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+    {
+        // create order entity from the command object
+        // save the database
+        // return the result
+        throw new NotImplementedException();
+    }
+}
