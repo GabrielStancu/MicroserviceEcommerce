0116aca94939c4e402a95c7eff22507471845b11
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-12-02 02:33:05 +0200

    Use pagination for get orders query

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,31 @@
+﻿using Core.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public GetOrdersHandler(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+        var totalCount = await _dbContext.Orders.LongCountAsync(cancellationToken);
+        var orders = await _dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .OrderBy(o => o.OrderName.Value)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersResult(
+            new PaginatedResult<OrderDto>(
+                pageIndex, pageSize, totalCount, orders.ToOrderDtoList()));
+    }
+}
