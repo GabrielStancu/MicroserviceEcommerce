1e4d486ac9e6ab9c1add07c5697bb51e0d907211
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-12-10 14:47:57 +0200

    Delete order endpoint implementation

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,5 +1,26 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
 
-public class DeleteOrder
+namespace Ordering.API.Endpoints;
+
+//public record DeleteOrderRequest(Guid Id);
+
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid id, ISender sender) =>
+            {
+                var command = new DeleteOrderCommand(id);
+                var result = await sender.Send(command);
+                var response = result.Adapt<DeleteOrderResponse>();
+
+                return Results.Ok(response);
+            }).WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("DeleteOrder")
+            .WithDescription("Delete Order");
+    }
 }
