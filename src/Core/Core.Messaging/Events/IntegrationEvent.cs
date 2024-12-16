3b31a4341d1aed049d5edb82070c1576f90f0606
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-12-16 18:03:29 +0200

    Messaging class library with event definitions

---
+++ src/Core/Core.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
