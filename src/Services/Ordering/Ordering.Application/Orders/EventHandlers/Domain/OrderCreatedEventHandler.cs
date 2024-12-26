7464556c0a4e6a1f60a91d8626f428d8836da2da
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-12-26 14:53:45 +0200

    Feature flag for enabling order fulfilment event publishing

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,16 +1,20 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 
 public class OrderCreatedEventHandler : INotificationHandler<OrderCreatedEvent>
 {
     private readonly IPublishEndpoint _publishEndpoint;
+    private readonly IFeatureManager _featureManager;
     private readonly ILogger<OrderCreatedEventHandler> _logger;
 
     public OrderCreatedEventHandler(IPublishEndpoint publishEndpoint,
+        IFeatureManager featureManager,
         ILogger<OrderCreatedEventHandler> logger)
     {
         _publishEndpoint = publishEndpoint;
+        _featureManager = featureManager;
         _logger = logger;
     }
 
@@ -18,8 +22,10 @@ public class OrderCreatedEventHandler : INotificationHandler<OrderCreatedEvent>
     {
         _logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
-
-        await _publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        if (await _featureManager.IsEnabledAsync("OrderFulfillment"))
+        {
+            var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
+            await _publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
     }
 }
